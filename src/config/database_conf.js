4d6aa92338ddf84dc18b876f2978d94546042388
fattaah99 <121369097+fattaah99@users.noreply.github.com>
2023-12-15 08:03:19 +0700

    change the database connection

--- src/config/database_conf.js
+++ src/config/database_conf.js
@@ -1,15 +1,6 @@
-// const mysql = require("mysql2");
-
-// const db_pool = mysql.createPool({
-//   host: "localhost",
-//   user: "root",
-//   password: "",
-//   database: "culturama",
-// });
-
 // module.exports = db_pool.promise();
 const { Sequelize } = require("sequelize");
-const sequelize = new Sequelize("culturama", "root", "", {
+const sequelize = new Sequelize("database", "user", "password", {
   host: "localhost",
   dialect: "mysql", // Sesuaikan dengan jenis database yang digunakan
 });
